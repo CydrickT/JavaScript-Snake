4894765064009e3b1bfdcd7bec8a86f826282406
Cydrick <cydtru@gmail.com>
2017-07-16 19:41:27 -0400

    Improvements

--- js/snake.js
+++ js/snake.js
@@ -1051,7 +1051,15 @@ SNAKE.Board = SNAKE.Board || (function () {
 
                 // Search for #listenerX to see where this is removed
                 SNAKE.addEventListener(elmContainer, "keydown", myKeyListener, false);
-                SNAKE.addEventListener(window, "devicemotion", myDeviceOrientationListener, false);
+                var gyroPresent = false;
+                SNAKE.addEventListener(window, "devicemotion", function(event){
+                    if(event.rotationRate.alpha || event.rotationRate.beta || event.rotationRate.gamma){
+                        SNAKE.addEventListener(window, "devicemotion", myDeviceOrientationListener, false);
+                        alert("Yes");
+                    }else{
+                        alert("No");
+                    }
+                });
             };
 
 
